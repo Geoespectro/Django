7f91326094653bb68101defc1e14d8030b42ae04
juan <carlonchoquinteros@gmail.com>
2024-03-27 20:17:14 -0300

    csss

--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from vistaprevia.models import Producto
-#from django.db.models import Q
+from django.db.models import Q
 
 def index(request):
     params = {}
     params['nombre_sitio'] = 'Libros Online'
-    productos = Producto.objects.filter(estado="Publicado")
+    productos = Producto.objects.filter(Q(estado="Publicado"))
     params['productos'] = productos
     return render(request, 'vistaprevia/index.html', params)
     
